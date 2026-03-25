22a66095af804467322aa9888e6b5c22ae478dab
Emre Sırmalı <lynicis@protonmail.com>
2026-03-25 13:59:25 +0300

    feat: add dependencies and configure extension permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,11 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  manifest: {
+    name: "Don't Let YouTube Track You",
+    description:
+      'Privately track your YouTube browsing history and sync across devices without Google tracking',
+    permissions: ['storage', 'unlimitedStorage', 'offscreen', 'tabs'],
+    host_permissions: ['https://*.supabase.co/*'],
+  },
 });
